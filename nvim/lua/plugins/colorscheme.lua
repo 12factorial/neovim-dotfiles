09d87d5d167a6d78d0d272a95ec3fa77f34f974f
12! <12factorial@posteo.net>
2026-01-27 23:09:21 +0100

    Switched to Catppuccin Mocha.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,14 @@
 vim.pack.add({
-    { src = 'https://github.com/sainnhe/gruvbox-material.git', name = 'gruvbox-material' },
+    { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
     { src = 'https://github.com/nvim-lualine/lualine.nvim.git' },
     { src = 'https://github.com/nvim-tree/nvim-web-devicons.git' }
 })
 
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_show_eob = 0
-vim.g.gruvbox_material_float_style = 'blend'
-vim.g.gruvbox_material_disable_terminal_colors = 1
-vim.cmd('colorscheme gruvbox-material')
+vim.cmd('colorscheme catppuccin-mocha')
 
 require('lualine').setup {
     options = {
-	theme = 'gruvbox-material',
+	theme = 'catppuccin-mocha',
 	component_separators = { left = '|', right = '|' },
 	section_separators = { left = '', right = '' },
 	disabled_filetypes = { 'alpha', 'help' }
@@ -24,7 +20,7 @@ require('nvim-web-devicons').setup({
     color_icons = false,
     override = {
 	["default_icon"] = {
-	    color = "#DDC7A1",
+	    -- color = "#DDC7A1",
 	    name = "Default"
 	}
     }
